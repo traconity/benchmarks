0a9e6a9a5ed3c7a87b24d912be4e0f7191178186
github-action-benchmark <github@users.noreply.github.com>
2022-06-25 16:45:03 +0000

    add Benchmark (benchmarkdotnet) benchmark result for f8beee34e31bae94b5116ea600c9e4f4724c6cd8

---
+++ data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1656175503072,
+  "repoUrl": "https://github.com/traconity/tracon",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "jjones@outlook.cz",
+            "name": "Jan Joneš",
+            "username": "jjonescz"
+          },
+          "committer": {
+            "email": "jjones@outlook.cz",
+            "name": "Jan Joneš",
+            "username": "jjonescz"
+          },
+          "distinct": true,
+          "id": "f8beee34e31bae94b5116ea600c9e4f4724c6cd8",
+          "message": "Execute only the default planning benchmark in CI",
+          "timestamp": "2022-06-25T18:29:13+02:00",
+          "tree_id": "e14a74fc36cfb7a4a786402f36e9c33c17a75401",
+          "url": "https://github.com/traconity/tracon/commit/f8beee34e31bae94b5116ea600c9e4f4724c6cd8"
+        },
+        "date": 1656175502807,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "Tracon.Benchmarks.PlanningBenchmark.CellList",
+            "value": 1298752590.5333333,
+            "unit": "ns",
+            "range": "± 3459112.964589392"
+          }
+        ]
+      }
+    ]
+  }
+}
