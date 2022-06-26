244dbf9f63ff74258fc374e54442a381c01fabc6
github-action-benchmark <github@users.noreply.github.com>
2022-06-26 08:52:54 +0000

    add Benchmark (benchmarkdotnet) benchmark result for 197517128cae756932c586814baf00c30271db4a

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1656175503072,
+  "lastUpdate": 1656233574509,
   "repoUrl": "https://github.com/traconity/tracon",
   "entries": {
     "Benchmark": [
@@ -30,6 +30,36 @@ window.BENCHMARK_DATA = {
             "value": 1298752590.5333333,
             "unit": "ns",
             "range": "± 3459112.964589392"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jjones@outlook.cz",
+            "name": "Jan Joneš",
+            "username": "jjonescz"
+          },
+          "committer": {
+            "email": "jjones@outlook.cz",
+            "name": "Jan Joneš",
+            "username": "jjonescz"
+          },
+          "distinct": true,
+          "id": "197517128cae756932c586814baf00c30271db4a",
+          "message": "Synchronize access of tests to feeds",
+          "timestamp": "2022-06-26T10:45:55+02:00",
+          "tree_id": "3204183b33a992955d77afdc06882575f96141a9",
+          "url": "https://github.com/traconity/tracon/commit/197517128cae756932c586814baf00c30271db4a"
+        },
+        "date": 1656233574250,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "Tracon.Benchmarks.PlanningBenchmark.CellList",
+            "value": 1301205529,
+            "unit": "ns",
+            "range": "± 4175422.8933279044"
           }
         ]
       }
